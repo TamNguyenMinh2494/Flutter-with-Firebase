170467cb8e0a71eaa8f254f8f80590dc5bd9179c
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-02-11 17:47:23 +0700

    User after authenticated

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,17 @@
+import 'package:firebaseapp/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'home/home.dart';
 import 'authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    final user = Provider.of<User>(context);
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
