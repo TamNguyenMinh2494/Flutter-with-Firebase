8217d7f4f696c6d161800e79e3f4c50ab8367d83
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-02-11 17:46:12 +0700

    created button signOut

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,27 @@
+import 'package:firebaseapp/screens/authenticate/authenticate.dart';
+import 'package:firebaseapp/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home page'),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Home Page'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text('Logout'))
+        ],
+      ),
     );
   }
 }
