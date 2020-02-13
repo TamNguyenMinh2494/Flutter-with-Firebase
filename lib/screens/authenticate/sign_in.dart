90aaaa91b784042ba204e81e89b367fda011f3b4
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-02-13 11:23:55 +0700

    Created UI for signin with spinkit

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,5 @@
+import 'package:firebaseapp/shared/constants.dart';
+import 'package:firebaseapp/shared/spinkit.dart';
 import 'package:flutter/material.dart';
 import 'package:firebaseapp/services/auth.dart';
 import 'package:firebaseapp/models/user.dart';
@@ -13,72 +15,84 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool spinkit = false;
   String email = '';
   String password = '';
   String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-        title: Text('Sign in'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Register'))
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    validator: (val) => val.isEmpty ? 'Enter your email' : null,
-                    onChanged: (val) {
-                      setState(() => email = val);
+    return spinkit
+        ? Spinkit()
+        : Scaffold(
+            backgroundColor: Colors.blue[100],
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              elevation: 0.0,
+              title: Text('Sign in'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    validator: (val) =>
-                        val.length < 6 ? 'Enter your password' : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result = await _auth.signInWithEmailandPassword(
-                            email, password);
-                        if (result == null) {
-                          setState(() =>
-                              error = 'Sign in failes. Please Try it again!!!');
-                        }
-                      }
-                    },
-                    color: Colors.pink[400],
-                    child:
-                        Text('Sign in', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  ),
-                ],
-              ))),
-    );
+                    icon: Icon(Icons.person),
+                    label: Text('Register'))
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'abc@gmail.com'),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter your email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Password',
+                          ),
+                          obscureText: true,
+                          validator: (val) =>
+                              val.length < 6 ? 'Enter your password' : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        RaisedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => spinkit = true);
+                              dynamic result = await _auth
+                                  .signInWithEmailandPassword(email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error = 'Signin failed!!!';
+                                  spinkit = false;
+                                });
+                              }
+                            }
+                          },
+                          color: Colors.pink[400],
+                          child: Text('Sign in',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        ),
+                      ],
+                    ))),
+          );
   }
 }
