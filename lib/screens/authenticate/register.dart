ad30e9222bb2f15a608b5e9d81a63521ed4e6523
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-02-13 11:23:27 +0700

    Created UI for register with spinkit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,6 @@
 import 'package:firebaseapp/services/auth.dart';
+import 'package:firebaseapp/shared/constants.dart';
+import 'package:firebaseapp/shared/spinkit.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -12,73 +14,87 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-
+  bool spinkit = false;
   String email = '';
   String password = '';
   String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign up'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Sign in'))
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    validator: (val) => val.isEmpty ? 'Enter your email' : null,
-                    onChanged: (val) {
-                      setState(() => email = val);
+    return spinkit
+        ? Spinkit()
+        : Scaffold(
+            backgroundColor: Colors.blue[100],
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              elevation: 0.0,
+              title: Text('Sign up'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    validator: (val) => val.length < 6
-                        ? 'Enter your password at least 6 characters'
-                        : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result = await _auth
-                            .registerWithEmailandPassword(email, password);
-                        if (result == null) {
-                          setState(() => error = 'Invalid email');
-                        }
-                      }
-                    },
-                    color: Colors.pink[400],
-                    child:
-                        Text('Register', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(height: 12.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  ),
-                ],
-              ))),
-    );
+                    icon: Icon(Icons.person),
+                    label: Text('Sign in'))
+              ],
+            ),
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'abc@gmail.com',
+                          ),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter your email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Password',
+                          ),
+                          obscureText: true,
+                          validator: (val) => val.length < 6
+                              ? 'Enter your password at least 6 characters'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        RaisedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() => spinkit = true);
+                              dynamic result =
+                                  await _auth.registerWithEmailandPassword(
+                                      email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error = 'Invalid email';
+                                  spinkit = false;
+                                });
+                              }
+                            }
+                          },
+                          color: Colors.pink[400],
+                          child: Text('Register',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        ),
+                      ],
+                    ))),
+          );
   }
 }
