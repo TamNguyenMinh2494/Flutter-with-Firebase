72a1d3b76385eeb24a2933b3bd6509111144cd04
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-02-18 12:10:17 +0700

    designed homePgae

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,26 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebaseapp/models/user.dart';
 import 'package:firebaseapp/screens/authenticate/authenticate.dart';
+import 'package:firebaseapp/screens/home/user_list.dart';
 import 'package:firebaseapp/services/auth.dart';
+import 'package:firebaseapp/services/database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Home Page'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logout'))
-        ],
+    return StreamProvider<List<User>>.value(
+      value: DatabaseService().users,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Home Page'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('Logout'))
+          ],
+        ),
+        body: UserList(),
       ),
     );
   }
